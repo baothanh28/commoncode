9fc427cb7093198f2c498a1fc610565d6b705c86
baothanh28 <55008588+baothanh28@users.noreply.github.com>
2021-04-01 03:09:45 +0800

    Update useExtend.js

--- testReactive 4/useExtend.js
+++ testReactive 4/useExtend.js
@@ -1,6 +1,7 @@
 import { toRefs, reactive } from "vue";
 export default function useExtend(proxyObj,obj){
     console.log('run')
+    //co the set default obj o day hoac th
     const newExtends=reactive(obj);
     //const ss=toRefs(obj);
     return { 
